61e5295d00a3fb9f60015383ed0006f1292e775c
bojanangelis <bojanangeleski98@gmail.com>
2023-02-04 17:16:51 +0100

    fixed amplify errors

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigatior from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import awsmobile from './src/aws-exports';
 
-export default function Native() {
-  // here i need to implement aws!
+Amplify.configure(awsmobile);
+
+const App = () => {
   return (
     <NavigationContainer>
       <RootNavigatior />
       <StatusBar style="dark" />
     </NavigationContainer>
   );
-}
+};
+
+export default App;
