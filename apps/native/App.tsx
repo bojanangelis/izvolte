e6c9c95beabd018ebf709a0681406441fc605a93
bojanangelis <bojanangeleski98@gmail.com>
2023-02-06 19:58:34 +0100

    WIP-signup

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -4,17 +4,19 @@ import RootNavigatior from './src/navigation';
 import { Amplify } from 'aws-amplify';
 import awsmobile from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
-import { signUpConfig } from './utils/signUp';
+import SignInScreen from './src/screens/SignInScreen';
+// import { signUpConfig } from './utils/signUp';
 
 Amplify.configure(awsmobile);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <RootNavigatior />
+      {/* <RootNavigatior /> */}
+      <SignInScreen />
       <StatusBar style="dark" />
     </NavigationContainer>
   );
 };
 
-export default withAuthenticator(App, { signUpConfig });
+export default withAuthenticator(App);
